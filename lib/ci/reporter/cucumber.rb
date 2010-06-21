30a84473848491ff17382e8448ba2ab800a110ed
Tim Felgentreff <timfelgentreff@gmail.com>
2010-06-21 15:55:58 +0200

    use new visitor API for cucumber

--- lib/ci/reporter/cucumber.rb
+++ lib/ci/reporter/cucumber.rb
@@ -6,7 +6,6 @@ require 'ci/reporter/core'
 tried_gem = false
 begin
   require 'cucumber'
-  require 'cucumber/ast/visitor'
 rescue LoadError
   unless tried_gem
     tried_gem = true
@@ -46,53 +45,56 @@ module CI
       end
     end
 
-    class Cucumber < ::Cucumber::Ast::Visitor
-
-      attr_accessor :test_suite, :report_manager, :feature_name
+    class Cucumber
 
       def initialize(step_mother, io, options)
-        self.report_manager = ReportManager.new("features")
-        super(step_mother)
+        @report_manager = ReportManager.new("features")
       end
 
-      def visit_feature_name(name)
-        self.feature_name = name.split("\n").first
-        super
+      def before_feature_name(name)
+        @current_feature_name = name.split("\n").first
       end
 
-      def visit_feature_element(feature_element)
-        self.test_suite = TestSuite.new("#{feature_name} #{feature_element.instance_variable_get("@name")}")
-        test_suite.start
-
-        return_value = super
-
-        test_suite.finish
-        report_manager.write_report(test_suite)
-        self.test_suite = nil
-
-        return_value
+      def before_feature_element(feature_element)
+        @test_suite = TestSuite.new("#{@current_feature_name} #{feature_element.instance_variable_get("@name")}")
+        @test_suite.start
       end
 
-      def visit_step(step)
-        test_case = TestCase.new(step.name)
-        test_case.start
+      def after_feature_element(feature_element)
+        @test_suite.finish
+        @report_manager.write_report(@test_suite)
+        @test_suite = nil
+      end
 
-        return_value = super
+      def before_step(step)
+        @test_case = TestCase.new(step.name)
+        @test_case.start
+      end
 
-        test_case.finish
+      def after_step(step)
+        if @test_case.nil? 
+          $stderr << "Warning, no test case was started for #{step}. Time won't be logged."
+          @test_case = TestCase.new(step.name)
+          @test_case.start
+        end
+        @test_case.finish
 
         case step.status
         when :pending, :undefined
-          test_case.name = "#{test_case.name} (PENDING)"
+          @test_case.name = "#{@test_case.name} (PENDING)"
         when :skipped
-          test_case.name = "#{test_case.name} (SKIPPED)"
+          @test_case.name = "#{@test_case.name} (SKIPPED)"
         when :failed
-          test_case.failures << CucumberFailure.new(step)
+          @test_case.failures << CucumberFailure.new(step)
         end
 
-        test_suite.testcases << test_case
-
-        return_value
+        if @test_suite.nil? 
+          $stderr << "Warning, no test suite was started for the scenario around #{step}. Time logging will be wrong."
+          @test_suite = TestSuite.new("#{@current_feature_name} Unspecified Feature Element")
+          @test_suite.start
+        end
+        @test_case.finish
+        @test_suite.testcases << @test_case
       end
     end
   end
